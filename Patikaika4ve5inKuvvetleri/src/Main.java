1c7aa709adb0a1abc8710f3983c0f312a0435142
tseyma <73405114+tseyma@users.noreply.github.com>
2023-03-10 21:17:09 +0300

    4 ve 5in Kuvvetleri

---
+++ Patikaika4ve5inKuvvetleri/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sınır sayısını giriniz");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i *= 4) {
+            System.out.println(i);
+        }
+        System.out.println("*************************************************");
+        System.out.println("Girdiğiniz sayıya kadar olan 5'in kuvvetleri:  ");
+
+        for (int j = 1; j <= n; j *= 5) {
+            System.out.println(k);
+
+        }
+
+
+    }
+}
