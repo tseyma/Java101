7897d4f19d1616e7934df920c73af1b17aa45eb7
tseyma <73405114+tseyma@users.noreply.github.com>
2023-04-05 22:03:53 +0300

    ArmstrongSayilar

---
+++ ArmstrongSayilar/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number, basValue, result =0;
+        System.out.print("Sayıyı Giriniz: ");
+        number = input.nextInt();
+
+        while (number!=0){
+            basValue=number%10;
+            result+=basValue;
+            number /=10;
+        }
+        System.out.print(result);
+
+
+    }
+}
