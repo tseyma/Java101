536b64d939601b9e5bd8d41b4306c9582dca1f6f
tseyma <73405114+tseyma@users.noreply.github.com>
2023-04-18 23:55:23 +0300

    Maas Hesaplayici

---
+++ MaasHesaplayici/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Şeyma", 100000, 50, 2023);
+
+        employee.toStringP();
+    }
+
+}
